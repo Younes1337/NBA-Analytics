81629274da46a27d3055719301ec5e5071ac7d8b
Younes Mamma <90696434+Younes1337@users.noreply.github.com>
2024-12-09 01:31:52 +0100

    Update APP.py
    Update Main APP

--- APP.py
+++ APP.py
@@ -5,6 +5,7 @@ from utils.utils import extract_money_lines, moneyline_to_probability, calculate
 from utils.DataProviders import SbrOddsProvider
 import pytz
 import time
+
 # Set Streamlit page config for better visuals
 st.set_page_config(page_title="NBA Live Scores / Predictions", page_icon="🏀", layout="wide")
 
@@ -55,7 +56,6 @@ def moneyline_data():
 def display_nba_live_data():
     placeholder = st.empty()  # Placeholder for dynamic table updates
 
-<<<<<<< HEAD
     # Get the odds
     sbr_provider = SbrOddsProvider()
     odds_df = sbr_provider.get_odds_table()
@@ -125,28 +125,10 @@ def display_nba_live_data():
                     """
                     <div style="text-align: center; font-size: 20px; margin-top: 20px; color: #FFD700;">
                          <strong>Sportsbook Odds</strong> 
-=======
-    while True:  # Continuous refresh
-        # Fetch live data
-        games_data = nba_live_data()
-        moneyline_df = moneyline_data()
-        games = games_data.get("scoreboard", {}).get("games", [])
-
-        # Update the placeholder content
-        with placeholder.container():
-            if games:
-                st.markdown(
-                    """
-                    <div style="text-align: center; font-size: 24px; margin-bottom: 20px; color: #ffffff;">
-                        <strong>Home Team  Vs  Away Team</strong>
->>>>>>> 4aad7ce4b59c7ed53b63085671f4bb38d2b8faf6
                     </div>
                     """,
                     unsafe_allow_html=True,
                 )
-
-<<<<<<< HEAD
-                # Display the DataFrame
                 st.table(odds_df)
 
             else:
@@ -154,83 +136,11 @@ def display_nba_live_data():
                     """
                     <div style="text-align: center; margin-top: 50px; color: #ffffff; font-size: 22px;">
                         <h4>🚫 No live NBA games available right now. 🚫</h4>
-=======
-                for game in games:
-                    home_team = game["homeTeam"]["teamName"]
-                    away_team = game["awayTeam"]["teamName"]
-                    home_score = game["homeTeam"]["score"]
-                    away_score = game["awayTeam"]["score"]
-                    status = game["gameStatusText"]
-                    home_full_name = f"{game['homeTeam']['teamCity']} {home_team}"
-                    away_full_name = f"{game['awayTeam']['teamCity']} {away_team}"
-                    home_moneyline = moneyline_df[moneyline_df["home"] == home_full_name]["odds.home.moneyLine"].values
-                    away_moneyline = moneyline_df[moneyline_df["away"] == away_full_name]["odds.away.moneyLine"].values
-                    home_moneyline = home_moneyline[0] if home_moneyline else "N/A"
-                    away_moneyline = away_moneyline[0] if away_moneyline else "N/A"
-                    home_probability = moneyline_to_probability(home_moneyline)
-                    away_probability = moneyline_to_probability(away_moneyline)
-                    home_price = calculate_price(home_probability)
-                    away_price = calculate_price(away_probability)
-
-                    def moneyline_color(moneyline):
-                        if moneyline == "N/A":
-                            return "white"
-                        elif int(moneyline) > 0:
-                            return "green"
-                        else:
-                            return "red"
-
-                    st.markdown(
-                        f"""
-                        <div style="border: 1px solid #555; border-radius: 10px; padding: 10px; margin-bottom: 15px; background-color: #000000; color: #ffffff; max-width: 600px; margin-left: auto; margin-right: auto;">
-                            <div style="display: flex; justify-content: space-between; align-items: center;">
-                                <div style="text-align: left;">
-                                    <h4 style="margin: 5px 0; color: #4CAF50;">{home_full_name}</h4>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Score: {home_score}</p>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Moneyline: <span style="color: {moneyline_color(home_moneyline)};">{home_moneyline}</span></p>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Implied Probability: {home_probability if home_probability else "N/A"}</p>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Price: {home_price}</p>
-                                </div>
-                                <div style="text-align: center; font-size: 14px; color: #bbbbbb;">
-                                    <p style="margin: 5px 0; font-weight: bold;">{status}</p>
-                                </div>
-                                <div style="text-align: right;">
-                                    <h4 style="margin: 5px 0; color: #FF5722;">{away_full_name}</h4>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Score: {away_score}</p>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Moneyline: <span style="color: {moneyline_color(away_moneyline)};">{away_moneyline}</span></p>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Implied Probability: {away_probability if away_probability else "N/A"}</p>
-                                    <p style="margin: 5px 0; font-size: 14px; color: #dddddd;">Price: {away_price}</p>
-                                </div>
-                            </div>
-                        </div>
-                        """,
-                        unsafe_allow_html=True,
-                    )
-
-                # Sportsbook tables
-                sportbooks = ["fanduel", "betmgm", "caesars",
-                              "draftkings", "bet365"]
-                for sportbook in sportbooks:
-                    st.write(f"Live data for {sportbook}")
-                    provider = SbrOddsProvider(sportbook)
-                    df = provider.get_odds_table()
-                    st.table(df)
-
-            else:
-                st.markdown(
-                    """
-                    <div style="text-align: center; margin-top: 30px; color: #ffffff;">
-                        <h4>No live NBA games available right now.</h4>
->>>>>>> 4aad7ce4b59c7ed53b63085671f4bb38d2b8faf6
                     </div>
                     """,
                     unsafe_allow_html=True,
                 )
-<<<<<<< HEAD
         time.sleep(60)
-=======
-        time.sleep(60)  # Refresh data every minute
->>>>>>> 4aad7ce4b59c7ed53b63085671f4bb38d2b8faf6
 
 
 if __name__ == "__main__":
